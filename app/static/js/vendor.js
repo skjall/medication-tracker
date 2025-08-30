a8db5299aaf5f34a8cf30f1c7582d60ef5582582
Jan Grossheim <Jan.Grossheim@tufin.com>
2025-08-30 16:59:29 +0200

    fix: update onboarding form to use scanned data for package identifiers

--- app/static/js/vendor.js
+++ app/static/js/vendor.js
@@ -1,6 +1,6 @@
 // Import Bootstrap CSS and JS
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+import * as bootstrap from 'bootstrap';
 
 // Import Font Awesome
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -13,5 +13,8 @@ window.$ = $;
 // Import Popper.js (required by Bootstrap)
 import * as Popper from '@popperjs/core';
 window.Popper = Popper;
+
+// Make Bootstrap available globally
+window.bootstrap = bootstrap;
 
 console.log('Vendor libraries loaded: Bootstrap, Font Awesome, jQuery');
